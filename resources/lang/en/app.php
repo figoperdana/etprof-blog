0bcadaa729c7f6f97d9aa4994048c25a15c4c963
figoperdana <192410102022@mail.unej.ac.id>
2022-09-05 05:22:24 +0700

    update

---
+++ resources/lang/en/app.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | English Application Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'buttons' => [
+        'share' => 'Share',
+        'tweet' => 'Tweet'
+    ],
+    'footer' => [
+        'about'  => 'About Us',
+        'latest' => 'Latest Articles',
+        'share'  => 'Spread the Word',
+        'text'   => 'This is CMS Blog for Profession Ethic Subject in my college',
+        'url'    => 'https://www.linkedin.com/in/figoperdana/'
+    ],
+    'pagination' => [
+        'next'     => 'Next &raquo;',
+        'previous' => '&laquo; Previous',
+    ],
+    'read_more'    => 'Read More'
+];
---
+++ resources/views/partials/app/hero.blade.php
@@ -0,0 +1,13 @@
+<section class="hero is-primary {{ $class ?? '' }}">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-12">
+                    <h1 class="title">Blog Etika Profesi</h1>
+                    <h2 class="subtitle">Figo Perdana Putra / 192410102022</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@include('partials.app.errors')
